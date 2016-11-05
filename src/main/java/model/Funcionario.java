f29b62269c8144e4909f8c0243e4c9785c2d4c92
Norbert Strzybny <nor_str@hotmail.com>
2016-11-04 23:23:25 -0200

    more stuff

---
+++ src/main/java/model/Funcionario.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author PC
+ */
+public class Funcionario extends Pessoa {
+    
+    private long idFuncionario;
+    private int matricula;
+    private String dataAdmissao;
+    private String cargo;
+    
+    
+
+    public long getIdFuncionario() {
+        return idFuncionario;
+    }
+
+    public void setIdFuncionario(long idFuncionario) {
+        this.idFuncionario = idFuncionario;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public void setDataAdmissao(String dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    
+    
+    
+}
--- src/main/java/model/Pessoa.java
+++ src/main/java/model/Pessoa.java
@@ -6,7 +6,45 @@ public class Pessoa {
     
     
     private long idPessoa;
+    private String nome;
+    private String cpf;
+    private String endereco;
+
+    public long getIdPessoa() {
+        return idPessoa;
+    }
+
+    public void setIdPessoa(long idPessoa) {
+        this.idPessoa = idPessoa;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
     
+    
+    
+        
     
     
 }
